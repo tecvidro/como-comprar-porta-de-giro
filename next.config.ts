c33ab5e7524886d70ac58ebe03ff86228f2af6bb
Thiago Grandjean <thigrandjean@yahoo.com.br>
2025-10-28 13:27:17 -0300

    feat: add first content

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['mdx', 'ts', 'tsx']
 };
 
 export default nextConfig;
--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -5,9 +5,9 @@ type HeaderProps = {
 }
 export function Header({ title }: HeaderProps) {
   return (
-    <header className="bg-dark-blue p-6 flex justify-between items-center">
+    <header className="bg-dark-blue p-6 flex justify-between items-center gap-8">
       <Logo />
-      <h1 className="font-bold text-gray-100 text-xl">{title}</h1>
+      <h1 className="font-bold text-gray-100 text-right">{title}</h1>
     </header>
   )
 }
