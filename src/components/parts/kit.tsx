19f0f79f1866d3acc0b767affbbdc66a55ca16e0
Thiago Grandjean <thigrandjean@yahoo.com.br>
2025-10-30 16:57:32 -0300

    feat: content responsiveness

--- src/components/parts/kit.tsx
+++ src/components/parts/kit.tsx
@@ -8,18 +8,18 @@ type KitProps = {
 
 export function Kit({ title, images }: KitProps) {
   return (
-    <div className="grid grid-cols-12 gap-4 items-center">
-      <div className="bg-gray-100 p-4 col-span-5 rounded flex flex-col items-center gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 items-center justify-center">
+      <div className="bg-gray-100 p-4 lg:col-span-5 rounded flex flex-col items-center gap-4">
         <h3 className="font-bold text-orange">
           {title}
         </h3>
         <img src="images/kits/desenho-kit-standard.png" width={349} height={742} />
       </div>
-      <div className="flex col-span-7 w-full justify-between items-center">
+      <div className="flex flex-col lg:flex-row lg:col-span-7 w-full justify-between items-center">
         {!!images && images.map((image) => (
-          <div key={image.alt} className="flex items-center justify-around w-full">
+          <div key={image.alt} className="flex flex-col lg:flex-row items-center justify-around w-full">
             <Signal type={image.signal} />
-            <img src={image.src} alt={image.alt} />
+            <img src={image.src} alt={image.alt} className="w-full" />
           </div>
         ))}
       </div>
@@ -29,9 +29,9 @@ export function Kit({ title, images }: KitProps) {
 
 function Signal({ type = 'plus' }: { type?: 'equal' | 'plus' }) {
   return (
-    <>
+    <div className="bg-gray-100">
       {type == 'plus' && <Plus />}
       {type == 'equal' && <Equal />}
-    </>
+    </div>
   )
 }
